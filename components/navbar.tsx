5d561801deefdda8159adbc1aa77e00665aadd94
Nabil <nabil.mahmoor@gmail.com>
2026-08-19 09:36:40 +0530

    Fix navbar transparency

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import { SidebarTrigger } from "./ui/sidebar";
 
 export default function Navbar() {
   return (
-    <nav className="p-2 flex items-center justify-left border-b border-primary-background sticky top-0 z-50">
+    <nav className="p-2 flex items-center justify-left border-b border-primary-background bg-primary-foreground sticky top-0 z-50">
       <SidebarTrigger className="cursor-pointer" />
     </nav>
   );
