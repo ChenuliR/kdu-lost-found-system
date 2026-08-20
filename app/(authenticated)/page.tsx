1df167bcf2921ae13e275ccd8aefd1816ce2c559
Nabil <nabil.mahmoor@gmail.com>
2026-08-20 08:56:52 +0530

    Create function to get authenticated user

--- app/(authenticated)/page.tsx
+++ app/(authenticated)/page.tsx
@@ -1,16 +1,10 @@
 import UserComponent from "@/components/user-component";
+import { getAuthUser } from "@/lib/auth/server";
 import { createSupabaseServerClient } from "@/lib/supabase/server-client";
 import { redirect } from "next/navigation";
 
 export default async function BrowsePage() {
-  const supabase = await createSupabaseServerClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user) {
-    redirect("/auth");
-  }
+  const user = await getAuthUser()
 
   return <UserComponent user={user} />;
 }
